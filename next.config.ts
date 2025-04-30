70a74c13f29aab7cb2a85999308e9a1a1b2ba48c
neeludvadia <neel.udvadia@gmail.com>
2025-04-30 16:36:57 +0530

    added backend url for images in next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["localhost","shopzee-frontend-ql931vvn6-neeludvadias-projects.vercel.app"], // Add "localhost" here
+    domains: ["localhost","shopzee-frontend-ql931vvn6-neeludvadias-projects.vercel.app","node-backend-eqh1.onrender.com"], // Add "localhost" here
   },
 };
 
